ac6b388bb3d472f064e411ad7e43076aaaafbcb5
Sérgio Bezerra <sergiobezerra02@gmail.com.br>
2020-06-30 16:10:24 -0300

    Primeira versão

---
+++ src/test/java/com/meuPrimeiroProjeto/MeuPrimeiroProjetoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meuPrimeiroProjeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuPrimeiroProjetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
